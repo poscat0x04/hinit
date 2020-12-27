33ddf813180488abcb9ffcf97bd0f84857e0858b
Poscat <poscat@mail.poscat.moe>
2020-12-27 12:27:01 +0800

    Use spdx-license from nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,17 +1,10 @@
 {
   description = "A generic project initialization tool written in Haskell";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.flake-utils.url = github:poscat0x04/flake-utils;
-  inputs.spdx-license = {
-    url = github:poscat0x04/spdx-license/0.1.0;
-    inputs = {
-      nixpkgs.follows = "/nixpkgs";
-      flake-utils.follows = "/flake-utils";
-    };
-  };
 
-  outputs = { self, nixpkgs, flake-utils, spdx-license, ... }: with flake-utils;
+  outputs = { self, nixpkgs, flake-utils, ... }: with flake-utils;
     eachDefaultSystem (
       system:
         let
@@ -25,11 +18,9 @@
     ) // {
       overlay = self: super:
         let
-          pkgs = spdx-license.overlay self super;
           hpkgs = super.haskellPackages.override {
             overrides = hself: hsuper: {
               haskeline = hsuper.haskeline_0_8_1_0;
-              spdx-license = pkgs.spdx-license;
             };
           };
           hinit-base = hpkgs.callCabal2nix "hinit" ./. {};
